eaef9a393375f4bdfa8c1950913ac9834a4f8554
Сарюна Батоболотова <batabolovasg@domain.local>
2025-12-29 00:42:16 +0300

    fix1

--- src/test_class_car.py
+++ src/test_class_car.py
@@ -2,19 +2,22 @@ import unittest
 from .car import Car
 
 class TestCase(unittest.TestCase):
-    def setUp(self):
+    def setUp(self):  # ← С большой U!
         self.car = Car(model="BMW X5", fuel_capacity=80)
+        # Нужно добавить начальное топливо!
+        self.car.refuel_car(10)
 
     def tearDown(self):
         pass
 
     def test_drive(self):
-        self.car.drive(20)
-        self.assertRaises(Exception, lambda: self.car.drive(80000))
+        self.car.drive(20)  # Это должно работать
+        # Проверяем что исключение бросается
+        self.assertRaises(Exception, lambda: self.car.drive(80000))  # ← assertRaises!
 
     def test_refuel(self):
         # Заправим 20 литров
         self.car.refuel_car(20)
         assert self.car.get_current_fuel_level() == 20
         # Проверим, что будет исключение, если перельем
-        self.assertRaises(Exception, lambda: self.car.refuel_car(80))
+        self.assertRaises(Exception, lambda: self.car.refuel_car(80))  # ← assertRaises!
